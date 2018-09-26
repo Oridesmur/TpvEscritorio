d2a9a943637bb513e5e7ea006d25d25c5cf8f2be
dani17pl <daniel.perezl@um.es>
2018-09-27 00:15:11 +0200

    minimos cambios

--- Front/TpvEscritorio/src/app/app.module.ts
+++ Front/TpvEscritorio/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { LateralMenuComponent } from './lateral-menu/lateral-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LateralMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule, 
+    MatAutocompleteModule,
+    MatIconModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/TpvEscritorio/src/app/lateral-menu/lateral-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterialModule} from '@angular/material';
+
+@Component({
+  selector: 'app-lateral-menu',
+  templateUrl: './lateral-menu.component.html',
+  styleUrls: ['./lateral-menu.component.css']
+})
+export class LateralMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
